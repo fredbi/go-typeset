7ab23539472ec21b0ba543f6ba38eaa490b4a9d3
Frederic BIDON <fredbi@yahoo.com>
2023-01-18 19:17:30 +0100

    fixup runes writer test
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- terminal/runes/runesio/writer_test.go
+++ terminal/runes/runesio/writer_test.go
@@ -12,16 +12,24 @@ func TestRunesWriter(t *testing.T) {
 
 	writer := AsRunesWriter(w)
 
-	res, err := writer.WriteRune('a')
-	require.NoError(t, err)
-	require.Equal(t, 1, res)
-	require.Equal(t, "a", w.String())
-	res, err = writer.WriteRunes([]rune{'a', 'b', 'c'})
-	require.Equal(t, "aabc", w.String())
-	require.NoError(t, err)
-	require.Equal(t, 1, res)
-	res, err = writer.WriteRunes([]rune{'é', '🏈'})
-	require.Equal(t, "aabcé🏈", w.String())
-	require.NoError(t, err)
-	require.Equal(t, 1, res)
+	t.Run("should write a single rune", func(t *testing.T) {
+		res, err := writer.WriteRune('a')
+		require.NoError(t, err)
+		require.Equal(t, 1, res)
+		require.Equal(t, "a", w.String())
+	})
+
+	t.Run("should write several runes", func(t *testing.T) {
+		res, err := writer.WriteRunes([]rune{'a', 'b', 'c'})
+		require.Equal(t, "aabc", w.String())
+		require.NoError(t, err)
+		require.Equal(t, 3, res)
+	})
+
+	t.Run("should report the size in bytes", func(t *testing.T) {
+		res, err := writer.WriteRunes([]rune{'é', '🏈'})
+		require.Equal(t, "aabcé🏈", w.String())
+		require.NoError(t, err)
+		require.Equal(t, 6, res)
+	})
 }
